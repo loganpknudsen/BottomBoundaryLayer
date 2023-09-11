63de8db312498324b42307c0883361e75f15c95e
loganpknudsen <logantheginjaninja@gmail.com>
2023-09-11 10:16:01 -0400

    Update PSI_b_change.jl

--- PSI_b_change.jl
+++ PSI_b_change.jl
@@ -45,8 +45,11 @@ V = BackgroundField(V_func, parameters=ps)
 B = BackgroundField(B_func, parameters=ps)
 
 # Boundary condition set up
-b_bc = GradientBoundaryCondition(ps.Nₒ^2)
-buoyancy_grad = FieldBoundaryConditions(top=b_bc,bottom=b_bc)
+buoyancy_bt(y,z)=ps.Nₒ^2*z-ps.S^2*y
+bound_b = ValueBoundaryCondition(buoyancy_bt)
+buoyancy_grad = FieldBoundaryConditions(top=bound_b,bottom=bound_b)
+# b_bc = GradientBoundaryCondition(ps.Nₒ^2)
+# buoyancy_grad = FieldBoundaryConditions(top=b_bc,bottom=b_bc)
 
 start_time = time_ns()
 
