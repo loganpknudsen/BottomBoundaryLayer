56fbabe7476b236d340baaeeae8ec7fcd6fb90b6
loganpknudsen <logantheginjaninja@gmail.com>
2023-10-24 10:19:52 -0400

    LK
    Run new tests

--- PSI_Base_Test.jl
+++ PSI_Base_Test.jl
@@ -1,37 +1,32 @@
 using Oceananigans
-using Oceananigans.AbstractOperations: @at, ∂x, ∂y, ∂z
-using Oceananigans.Grids: Center, Face
 using Random
 using Printf
 using ArgParse
-using CUDA: has_cuda_gpu
-using Oceanostics
 
-function parse_commandline()
-    s = ArgParseSettings()
-    @add_arg_table s begin
-        "path"
-        help = "pathname to save data under"
-        default = ""
-    end
-    return parse_args(s)
-end
+# function parse_commandline()
+#     s = ArgParseSettings()
+#     @add_arg_table s begin
+#         "path"
+#         help = "pathname to save data under"
+#         default = ""
+#     end
+#     return parse_args(s)
+# end
 
-args=parse_commandline()
+# args=parse_commandline()
 
-@info("command line args:")
-for (arg,val) in args
-  @info("   $arg => $val")
-end
+# @info("command line args:")
+# for (arg,val) in args
+#   @info("   $arg => $val")
+# end
 
-path_name = args["path"]
+# path_name = args["path"]
 
 # made grid correct shape, need to modify z boundaries to make sure they are no slip
 
 # grid specifications
-arch = has_cuda_gpu() ? GPU() : CPU()
 
-grid = RectilinearGrid(arch; size=(1024, 200), y=(0,3000),z=(-200,0), topology=(Flat, Periodic, Bounded))
+grid = RectilinearGrid(size=(200, 200), y=(0,200),z=(-200,0), topology=(Flat, Periodic, Bounded))
 
 # realustuc mid latitude for now
 coriolis = FPlane(rotation_rate=7.292115e-5, latitude=45)
@@ -53,15 +48,14 @@ B = BackgroundField(B_func, parameters=ps)
 
 # Boundary condition set up
 
-b_bc = GradientBoundaryCondition(ps.Nₒ^2)
-buoyancy_grad = FieldBoundaryConditions(top=b_bc,bottom=b_bc)
+# b_bc = GradientBoundaryCondition(ps.Nₒ^2)
+# buoyancy_grad = FieldBoundaryConditions(top=b_bc,bottom=b_bc)
 # boundary_conditions=(;b=buoyancy_grad),
 # 
 
 start_time = time_ns()
 
 model = NonhydrostaticModel(; grid,
-                            boundary_conditions=(;b=buoyancy_grad),
                             coriolis,
                             advection = CenteredFourthOrder(),
                             timestepper = :RungeKutta3,
@@ -79,7 +73,7 @@ w₀(x, y, z) = ns*Random.randn()
 
 set!(model, u=u₀, v=v₀, w=w₀)
 
-simulation = Simulation(model, Δt = 1, stop_time = 20*(2*pi)/ps.f)
+simulation = Simulation(model, Δt = 1, stop_time = 1000)
 
 
 wizard = TimeStepWizard(cfl=0.5, max_change=1.1, max_Δt=10.0, min_Δt=0.001) 
@@ -90,22 +84,23 @@ progress_message(sim) =
         sim.model.clock.iteration, prettytime(sim.model.clock.time),
         prettytime(sim.Δt), prettytime((time_ns() - start_time) * 1e-9))
 
-simulation.callbacks[:progress] = Callback(progress_message, TimeInterval(1*(2*pi)/ps.f))
+simulation.callbacks[:progress] = Callback(progress_message, TimeInterval(100))
 
 # and add an output writer that saves the vertical velocity field every two iterations:
 
 u,v,w = model.velocities
 
-output = (;u,v,w,model.tracers.b,U=(model.background_fields.velocities.u+0*u),V=(model.background_fields.velocities.v+0*v),B=(model.background_fields.tracers.b+0*model.tracers.b))
+output = (;u,v,w,model.tracers.b,U=model.background_fields.velocities.u,V=model.background_fields.velocities.v,B=model.background_fields.tracers.b)
+# output = (;u,v,w,model.tracers.b,U=(model.background_fields.velocities.u+0*u),V=(model.background_fields.velocities.v+0*v),B=(model.background_fields.tracers.b+0*model.tracers.b))
 
-ε = Field(KineticEnergyDissipationRate(model))
-dBdz = Field(@at (Center, Center, Center) ∂z(model.tracers.b+model.background_fields.tracers.b))
+# ε = Field(KineticEnergyDissipationRate(model))
+# dBdz = Field(@at (Center, Center, Center) ∂z(model.tracers.b+model.background_fields.tracers.b))
 
-output = merge(output, (; E=ε, N2=dBdz,))
+# output = merge(output, (; E=ε, N2=dBdz,))
 
 simulation.output_writers[:fields] = NetCDFOutputWriter(model, output;
-                                                          schedule = TimeInterval(0.05*(2*pi)/ps.f),
-                                                          filename = path_name*"psi_base_test_ocng_w_b.nc",
+                                                          schedule = TimeInterval(100),
+                                                          filename = "psi_base_ocng_test.nc",
                                                           overwrite_existing = true)
 
 # With initial conditions set and an output writer at the ready, we run the simulation
