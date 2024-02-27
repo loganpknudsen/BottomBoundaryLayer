8fe0bb25ddb717ea29cfc674dc4a231ea233f6ee
loganpknudsen <logantheginjaninja@gmail.com>
2024-02-26 19:09:01 -0500

    Update BBL_with_oscillations_code.jl

--- BBL_with_oscillations_code.jl
+++ BBL_with_oscillations_code.jl
@@ -139,7 +139,7 @@ w₀(x, y, z) = ns*Random.randn()
 
 set!(model, u=u₀, v=v₀, w=w₀)
 
-simulation = Simulation(model, Δt = 0.5 * minimum_zspacing(grid) / (abs(V∞)), stop_time = 20*(2*pi)/ps.f)
+simulation = Simulation(model, Δt = 1, stop_time = 20*(2*pi)/ps.f)
 
 
 wizard = TimeStepWizard(cfl=0.7, max_change=1.1, max_Δt=10.0, min_Δt=0.001) 
