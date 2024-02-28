19f4caa6486d86f310ccb4493ef5ca8915b4a998
loganpknudsen <logantheginjaninja@gmail.com>
2024-02-28 13:29:14 -0500

    Update BBL_with_oscillations_code.jl

--- BBL_with_oscillations_code.jl
+++ BBL_with_oscillations_code.jl
@@ -131,7 +131,7 @@ start_time = time_ns()
 
 model = NonhydrostaticModel(; grid, buoyancy, coriolis, closure,
                             timestepper = :RungeKutta3,
-                            advection = UpwindBiasedFifthOrder(),
+                            advection = WENO(),
                             tracers = :b,
                             boundary_conditions = (; u=u_bcs, v=v_bcs),
                             background_fields = (; u=U_field, v=V_field, b=B_field))
