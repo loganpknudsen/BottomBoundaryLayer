6647d0f80017c12caae76562f28990d5f793a23f
loganpknudsen <logantheginjaninja@gmail.com>
2024-02-28 13:14:43 -0500

    Update BBL_with_oscillations_code.jl

--- BBL_with_oscillations_code.jl
+++ BBL_with_oscillations_code.jl
@@ -93,14 +93,16 @@ function interval(q,a,b)
     end
 end
 
-u_adjustment(x, y, z, t, p) = (p.uₒ*cos(p.fˢ*t)+sin(p.fˢ*t)*(p.f*p.vₒ-p.θ^2*p.Nₒ)/p.fˢ)*(p.hu-z)*interval(z,0,abs(p.hu))
-v_adjustment(x, y, z, t, p) = (p.vₒ-(p.f*p.uₒ)/p.fˢ*sin(p.fˢ*t)+(cos(p.fˢ*t)-1)*(p.f*p.vₒ-p.θ^2*p.Nₒ)/p.fˢ-sign(p.V∞)*(p.θ * p.N²)/(p.f))*(p.hu-z)*interval(z,0,abs(p.hu))*p.γ+p.V∞
-constant_stratification(x, y, z, t, p) = p.N² * x*p.ĝ[1] + p.N²*z*interval(z,abs(p.hu),p.Lz) + ((p.Nₒ*((1-(cos(p.fˢ*t)-1)*(p.θ^2*p.N²)/(p.fˢ)^2))+p.θ*p.N²*((p.uₒ)/p.fˢ*sin(p.fˢ*t)-(cos(p.fˢ*t)-1)*(p.f*p.vₒ)/(p.fˢ)^2))*(z+(sign(p.V∞)*p.θ*p.γ*p.hu)/(1-sign(p.V∞)*p.θ*p.γ)))*interval(z,0,abs(p.hu))
+@inline sn_fn(t,p) = sin(p.fˢ*t)
+@inline cs_fn(t,p) = cos(p.fˢ*t)
 
-B_field = BackgroundField(constant_stratification, parameters=p)
-V_field = BackgroundField(v_adjustment, parameters=p)
+u_adjustment(x, y, z, t, p) = (p.uₒ*cs_fn(t,p)+sn_fn(t,p)*(p.f*p.vₒ-p.θ^2*p.Nₒ)/p.fˢ)*(p.hu-z)*interval(z,0,abs(p.hu))
+v_adjustment(x, y, z, t, p) = (p.vₒ-(p.f*p.uₒ)/p.fˢ*sn_fn(t,p)+(cs_fn(t,p)-1)*(p.f*p.vₒ-p.θ^2*p.Nₒ)/p.fˢ-sign(p.V∞)*(p.θ * p.N²)/(p.f))*(p.hu-z)*interval(z,0,abs(p.hu))*p.γ+p.V∞
+constant_stratification(x, y, z, t, p) = p.N²*x*p.ĝ[1] + p.N²*z*interval(z,abs(p.hu),p.Lz) + ((p.Nₒ*((1-(cs_fn(t,p)-1)*(p.θ^2*p.N²)/(p.fˢ)^2))+p.θ*p.N²*((p.uₒ)/p.fˢ*sn_fn(t,p)-(cs_fn(t,p)-1)*(p.f*p.vₒ)/(p.fˢ)^2))*(z+(sign(p.V∞)*p.θ*p.γ*p.hu)/(1-sign(p.V∞)*p.θ*p.γ)))*interval(z,0,abs(p.hu))
+
 U_field = BackgroundField(u_adjustment, parameters=p)
-
+V_field = BackgroundField(v_adjustment, parameters=p)
+B_field = BackgroundField(constant_stratification, parameters=p)
 
 # Boundary condition set up
 
