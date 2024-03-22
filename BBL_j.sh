586762562bf86851d2bf88b568db5d8d27a025c1
loganpknudsen <logantheginjaninja@gmail.com>
2024-03-22 18:06:57 -0400

    Update BBL_j.sh

--- BBL_j.sh
+++ BBL_j.sh
@@ -25,7 +25,7 @@ module load ncarenv/23.10 gcc/12.2.0
 module load ncarcompilers/1.0.0
 module load netcdf/4.9.2 openmpi/4.1.6 
 module load julia
-module load cuda/12.2.1
+module load cuda/11.8.0
 
 ### file to run
 julia --project BBL_with_jump.jl /glade/derecho/scratch/knudsenl/BottomBoundaryLayer/
