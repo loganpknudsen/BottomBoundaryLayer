24c55863b5737557715efef96993ee738c78ac18
loganpknudsen <logantheginjaninja@gmail.com>
2024-04-03 13:57:41 -0400

    Update BBL_with_jump.jl

--- BBL_with_jump.jl
+++ BBL_with_jump.jl
@@ -67,6 +67,8 @@ grid = RectilinearGrid(arch; topology = (Periodic, Flat, Bounded),
 # ĝ = [θ, 0, 1] # gravity vector small angle
 ĝ = [sind(θ), 0, cosd(θ)] # gravity vector
 
+gs  = ĝ[1]
+gc = ĝ[3]
 # realustic mid latitude for now
 buoyancy = Buoyancy(model = BuoyancyTracer(), gravity_unit_vector = -ĝ)
 coriolis = ConstantCartesianCoriolis(f = 1e-4, rotation_axis = ĝ)
@@ -86,15 +88,15 @@ fˢ=(f^2+θ^2*N²)^(0.5)
 ε = V∞ #V∞ # adjustment parameter
 V∞a = V∞ + ε # m s⁻¹ # m s⁻¹
 
-p =(; N²,θ,f,V∞,hu,γ,uₒ,vₒ,Nₒ,fˢ,Lz,V∞a,ĝ)
+p =(; N²,θ,f,V∞,hu,γ,uₒ,vₒ,Nₒ,fˢ,Lz,V∞a,gs,gc)
 
 # background flow with geostrophic and ageostrophic shear 
 
 @inline interval(x,a,b) = ifelse(a<=x<=b, one(x), zero(x))
 
 u_adjustment(x, z, t, p) = p.uₒ
-v_adjustment(x, z, t, p) = -p.γ*(p.θ * p.N²)/(p.f)*(p.hu-z)*interval(z,0,abs(p.hu))*p.γ+p.V∞a
-constant_stratification(x, z, t, p) = p.N²*x*p.ĝ[1] + p.N²*z*p.ĝ[3]*interval(z,abs(p.hu),p.Lz) - p.N²*p.γ*(p.hu-z)*interval(z,0,abs(p.hu))
+v_adjustment(x, z, t, p) = -p.γ*(p.gs * p.N²)/(p.f)*(p.hu-z)*interval(z,0,abs(p.hu))*p.γ+p.V∞a
+constant_stratification(x, z, t, p) = p.N²*x*p.gs + p.N²*z*p.gc*interval(z,abs(p.hu),p.Lz) - p.N²*p.γ*(p.hu-z)*interval(z,0,abs(p.hu))
 
 U_field = BackgroundField(u_adjustment, parameters=p)
 V_field = BackgroundField(v_adjustment, parameters=p)
