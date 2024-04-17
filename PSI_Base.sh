22793db53b76100b75622816d8879c1af7311988
loganpknudsen <logantheginjaninja@gmail.com>
2024-04-17 13:39:12 -0400

    Update PSI_Base.sh

--- PSI_Base.sh
+++ PSI_Base.sh
@@ -20,13 +20,14 @@
 #PBS -m abe
 
 ## Clear environent and Load all the modules needed
-module purge
-module load ncarenv/23.10 ncarcompilers/1.0.0
+module --force purge
+module load ncarenv/23.10 gcc/12.2.0
+module load ncarcompilers/1.0.0
 module load netcdf/4.9.2 openmpi/4.1.6 
 module load julia
-module load cuda/12.2.1
+module load cuda/11.8.0
 
 ### file to run
-julia --project PSI_Base.jl /glade/scratch/knudsenl/BottomBoundaryLayer/
+julia --project PSI_Base.jl /glade/derecho/scratch/knudsenl/BottomBoundaryLayer
 
 
