4939bee66abbc98e9d1e9afddc2fb5293fe1c2c9
loganpknudsen <logantheginjaninja@gmail.com>
2024-04-23 19:40:13 -0400

    Summary

--- parameter_adjustment_file.jl
+++ parameter_adjustment_file.jl
@@ -20,6 +20,8 @@ fˢ=(f^2+θ^2*N²)^(0.5)
 uₒ = 0#γ*(N²*θ)/(f)*cos(ϕ)
 vₒ = γ*(N²*θ)/(f)*0.1#*sin(ϕ)
 bₒ = vₒ*((θ*N²)/(f)) # initial stratification
+q = vₒ*(θ*N²)
 println("$(hu)")
 println("$((f^2)/(N²*(1-γ)^2))")
 println("$(bₒ)")
+println("$(q)")
