f36c6118a8d0f7f498adeb2f814d83a5c37c926b
loganpknudsen <logantheginjaninja@gmail.com>
2024-04-24 22:40:17 -0400

    Update BBL_with_oscillations_code_GPU_check.jl

--- BBL_with_oscillations_code_GPU_check.jl
+++ BBL_with_oscillations_code_GPU_check.jl
@@ -80,8 +80,8 @@ p =(;N²,θ,f,V∞,hu,γ,uₒ,vₒ,bₒ,fˢ)
 # b_pert(x,z,t,p) = p.N²*p.θ*(p.f*p.vₒ+p.bₒ*p.θ)/(p.fˢ)^2*cs_fn(x,z,t,p) - (p.N²*p.θ*p.uₒ)/(p.fˢ)*sn_fn(x,z,t,p)+p.bₒ-((p.N²*p.θ^2)*p.bₒ+p.f*p.vₒ*p.θ*p.N²)/(p.fˢ)^2
 
 u_adjustment(x, z, t, p) =  0#u_pert(x,z,t,p)*(p.hu-z)*interval(z,0,p.hu)
-v_adjustment(x, z, t, p) = p.V∞#-p.γ*(p.θ * p.N²)/(p.f)*(p.hu-z)*interval(z,0,p.hu) + v_pert(x,z,t,p)*(p.hu-z)*interval(z,0,p.hu)
-constant_stratification(x, z, t, p) = p.N²*x*p.θ + p.N²*z #+ p.N²*p.γ*(p.hu-z)*interval(z,0,p.hu) + b_pert(x,z,t,p)*(p.hu-z)*interval(z,0,p.hu)
+v_adjustment(x, z, t, p) = p.V∞-p.γ*(p.θ * p.N²)/(p.f)*(p.hu-z)*interval(z,0,p.hu)# + v_pert(x,z,t,p)*(p.hu-z)*interval(z,0,p.hu)
+constant_stratification(x, z, t, p) = p.N²*x*p.θ + p.N²*z + p.N²*p.γ*(p.hu-z)*interval(z,0,p.hu) #+ b_pert(x,z,t,p)*(p.hu-z)*interval(z,0,p.hu)
 
 U_field = BackgroundField(u_adjustment, parameters=p)
 V_field = BackgroundField(v_adjustment, parameters=p)
