8c1fe906391733946105c5e18c2f5c98132a97d3
loganpknudsen <logantheginjaninja@gmail.com>
2024-07-08 12:47:43 -0400

    Update BBL_with_oscillations_code_10_percent.jl

--- BBL_with_oscillations_code_10_percent.jl
+++ BBL_with_oscillations_code_10_percent.jl
@@ -145,7 +145,8 @@ simulation.callbacks[:progress] = Callback(progress_message, IterationInterval(1
   return heaviside(hu-z)
 end
 full_mask(x,z) = bottom_mask(x,z)
-mask = Oceananigans.Fields.FunctionField{Center,Center,Center}(full_mask,model.grid)
+umask = Oceananigans.Fields.FunctionField{Face,Center,Center}(full_mask,model.grid)
+vmask = Oceananigans.Fields.FunctionField{Center,Face,Center}(full_mask,model.grid)
 ua, va, wa = model.velocities
 um = Field(@at (Center, Center, Center) Average(ua, dims=1))
 vm = Field(@at (Center, Center, Center) Average(va, dims=1))
@@ -193,14 +194,14 @@ dwdz = Field(@at (Center, Center, Center) ∂z(wpx))
 # zC = znodes(grid, Center())
 # @inline builder(z,h) = permutedims(heaviside(h.*ones(100,)-z).*ones(100,1,500),(3,2,1))
 # const hv = builder(zC,hu)
-WSPu = (u*w)*(u_pert(0,0,simulation.model.clock.time,p))*mask #.*hv  # AGSP contribution 
-WSPv = (v*w)*(v_pert(0,0,simulation.model.clock.time,p))*mask #.*hv
+WSPu = (u*w)*(u_pert(0,0,simulation.model.clock.time,p))*umask #.*hv  # AGSP contribution 
+WSPv = (v*w)*(v_pert(0,0,simulation.model.clock.time,p))*vmask #.*hv
 AGSPu = -1*(u*w)*(dudz) # AGSP contribution 
 AGSPv = -1*(v*w)*(dvdz)
 AGSPw = -1*(w*w)*(dwdz) #+θ*(wh*wh)*(u_pert(0,0,simulation.model.clock.time,p))
 WSP = WSPu + WSPv# + AGSPw
 AGSP = AGSPu + AGSPv + AGSPw
-GSP = -1*(v*w)*(γ*(θ * N²)/(f))*mask#.*hv # geostrophic shear production
+GSP = -1*(v*w)*(γ*(θ * N²)/(f))*vmask#.*hv # geostrophic shear production
 BFLUX = (w+u*θ)*b # flux from buoyancy
 # dpudx = Field(@at (Center, Center, Center) ∂z(θ*pr*u))
 # dpvdy = Field(@at (Center, Center, Center) ∂y(pr*v))
