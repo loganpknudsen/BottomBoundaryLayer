fce5f025cf1725ef4bd8ddf67267ebcdf4128ebb
loganpknudsen <logantheginjaninja@gmail.com>
2024-07-16 14:34:16 -0400

    Test Code

---
+++ test_code.sh
@@ -0,0 +1,33 @@
+#!/bin/bash -l
+### Job Name
+#PBS -N BBL_w_o_10
+### Project Code Allocation
+#PBS -A UMCP0023
+### Resources
+#PBS -l select=1:ncpus=1:ngpus=1
+### Run Time
+#PBS -l walltime=20:00:00
+### To the casper queue
+#PBS -q casper
+### output
+#PBS -o BBL_O_10.out
+### error
+#PBS -j oe
+### type of GPU
+#PBS -l gpu_type=v100 
+### email 
+#PBS -M knudsen@umd.edu
+#PBS -m abe
+
+## Clear environent and Load all the modules needed
+module --force purge
+module load ncarenv/23.10 gcc/12.2.0
+module load ncarcompilers/1.0.0
+module load netcdf/4.9.2 openmpi/4.1.6 
+module load julia/1.9.2
+module load cuda/12.2.1
+
+### file to run
+julia --project testcode.jl /glade/derecho/scratch/knudsenl/BottomBoundaryLayer/
+
+
---
+++ testcode.jl
@@ -0,0 +1,15 @@
+### Load in Packages
+# using Pkg
+# Pkg.instantiate()
+using Oceananigans
+# using Oceananigans.AbstractOperations: @at, ∂x, ∂y, ∂z
+# using Oceananigans.AbstractOperations: @at, Average
+# using Oceananigans.Grids: Center, Face
+# using Oceananigans.Units
+# using Random
+# using Printf
+# using ArgParse
+# using CUDA: has_cuda_gpu
+# using CUDA 
+# using Oceanostics
+
