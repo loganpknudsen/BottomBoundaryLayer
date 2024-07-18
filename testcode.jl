ec5dd52aca3d21297d63980787fc5e610d2acdcf
loganpknudsen <logantheginjaninja@gmail.com>
2024-07-18 13:27:50 -0400

    Update testcode.jl

--- testcode.jl
+++ testcode.jl
@@ -1,12 +1,12 @@
 using Oceananigans
-using Oceananigans.AbstractOperations: @at, ∂x, ∂y, ∂z
-using Oceananigans.AbstractOperations: @at, Average
-using Oceananigans.Grids: Center, Face
-using Oceananigans.Units
-using Random
-using Printf
-using ArgParse
-using CUDA: has_cuda_gpu
-using CUDA 
-using Oceanostics
+# using Oceananigans.AbstractOperations: @at, ∂x, ∂y, ∂z
+# using Oceananigans.AbstractOperations: @at, Average
+# using Oceananigans.Grids: Center, Face
+# using Oceananigans.Units
+# using Random
+# using Printf
+# using ArgParse
+# using CUDA: has_cuda_gpu
+# using CUDA 
+# using Oceanostics
 
