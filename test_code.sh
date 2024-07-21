75afdd4fce46595e9329302159cf4b2ff5e2a367
loganpknudsen <logantheginjaninja@gmail.com>
2024-07-21 19:55:06 -0400

    Update test_code.sh

--- test_code.sh
+++ test_code.sh
@@ -24,10 +24,10 @@ module --force purge
 module load ncarenv/23.10 gcc/12.2.0
 module load ncarcompilers/1.0.0
 module load netcdf/4.9.2 openmpi/4.1.6 
-module load julia@v1.10.4
+module load julia
 module load cuda/11.8.0
 
 ### file to run
-julia --project="/glade/derecho/scratch/knudsenl/BottomBoundaryLayer/.juliaup/bin/julia" testcode.jl /glade/derecho/scratch/knudsenl/BottomBoundaryLayer/
+julia --project="/glade/derecho/scratch/knudsenl/BottomBoundaryLayer/.juliaup/bin" testcode.jl /glade/derecho/scratch/knudsenl/BottomBoundaryLayer/
 
 
