d2262b0c7ef97bca5c6c009c34693be49c6996ba
loganpknudsen <logantheginjaninja@gmail.com>
2024-07-21 21:00:49 -0400

    Update test_code.sh

--- test_code.sh
+++ test_code.sh
@@ -25,7 +25,7 @@ module load ncarenv/23.10 gcc/12.2.0
 module load ncarcompilers/1.0.0
 module load netcdf/4.9.2 openmpi/4.1.6 
 module load julia/1.10
-module load cuda/11.8.0
+module load cuda/12.2.1
 
 ### file to run
 julia --project testcode.jl /glade/derecho/scratch/knudsenl/BottomBoundaryLayer/
