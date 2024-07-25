196b21ce15845c507a808a6c4c99e3bdb74767c6
loganpknudsen <logantheginjaninja@gmail.com>
2024-07-25 13:49:29 -0400

    Create ocean_mxing_and_convection.jl

---
+++ newtest/ocean_mxing_and_convection.jl
@@ -0,0 +1,90 @@
+using Random
+using Printf
+
+using Oceananigans
+using Oceananigans.Units: minute, minutes, hour
+
+Nx = Ny = 32     # number of points in each of horizontal directions
+Nz = 24          # number of points in the vertical direction
+
+Lx = Ly = 64     # (m) domain horizontal extents
+Lz = 32          # (m) domain depth
+
+refinement = 1.2 # controls spacing near surface (higher means finer spaced)
+stretching = 12  # controls rate of stretching at bottom
+
+# Normalized height ranging from 0 to 1
+h(k) = (k - 1) / Nz
+
+# Linear near-surface generator
+ζ₀(k) = 1 + (h(k) - 1) / refinement
+
+# Bottom-intensified stretching function
+Σ(k) = (1 - exp(-stretching * h(k))) / (1 - exp(-stretching))
+
+# Generating function
+z_faces(k) = Lz * (ζ₀(k) * Σ(k) - 1)
+
+grid = RectilinearGrid(size = (Nx, Nx, Nz),
+                          x = (0, Lx),
+                          y = (0, Ly),
+                          z = z_faces)
+buoyancy = SeawaterBuoyancy(equation_of_state=LinearEquationOfState(thermal_expansion = 2e-4,
+                          haline_contraction = 8e-4))
+Qʰ = 200.0  # W m⁻², surface _heat_ flux
+ρₒ = 1026.0 # kg m⁻³, average density at the surface of the world ocean
+cᴾ = 3991.0 # J K⁻¹ kg⁻¹, typical heat capacity for seawater
+
+Qᵀ = Qʰ / (ρₒ * cᴾ) # K m s⁻¹, surface _temperature_ flux
+dTdz = 0.01 # K m⁻¹
+
+T_bcs = FieldBoundaryConditions(top = FluxBoundaryCondition(Qᵀ),
+                                bottom = GradientBoundaryCondition(dTdz))
+                                u₁₀ = 10    # m s⁻¹, average wind velocity 10 meters above the ocean
+cᴰ = 2.5e-3 # dimensionless drag coefficient
+ρₐ = 1.225  # kg m⁻³, average density of air at sea-level
+
+Qᵘ = - ρₐ / ρₒ * cᴰ * u₁₀ * abs(u₁₀) # m² s⁻²
+u_bcs = FieldBoundaryConditions(top = FluxBoundaryCondition(Qᵘ))
+@inline Qˢ(x, y, t, S, evaporation_rate) = - evaporation_rate * S # [salinity unit] m s⁻¹
+evaporation_rate = 1e-3 / hour # m s⁻¹
+S_bcs = FieldBoundaryConditions(top=evaporation_bc)
+model = NonhydrostaticModel(; grid, buoyancy,
+                            advection = UpwindBiasedFifthOrder(),
+                            timestepper = :RungeKutta3,
+                            tracers = (:T, :S),
+                            coriolis = FPlane(f=1e-4),
+                            closure = AnisotropicMinimumDissipation(),
+                            boundary_conditions = (u=u_bcs, T=T_bcs, S=S_bcs))
+# Random noise damped at top and bottom
+Ξ(z) = randn() * z / model.grid.Lz * (1 + z / model.grid.Lz) # noise
+
+# Temperature initial condition: a stable density gradient with random noise superposed.
+Tᵢ(x, y, z) = 20 + dTdz * z + dTdz * model.grid.Lz * 1e-6 * Ξ(z)
+
+# Velocity initial condition: random noise scaled by the friction velocity.
+uᵢ(x, y, z) = sqrt(abs(Qᵘ)) * 1e-3 * Ξ(z)
+
+# `set!` the `model` fields using functions or constants:
+set!(model, u=uᵢ, w=uᵢ, T=Tᵢ, S=35)
+simulation = Simulation(model, Δt=10.0, stop_time=40minutes)
+wizard = TimeStepWizard(cfl=1.0, max_change=1.1, max_Δt=1minute)
+simulation.callbacks[:wizard] = Callback(wizard, IterationInterval(10))
+# Print a progress message
+progress_message(sim) = @printf("Iteration: %04d, time: %s, Δt: %s, max(|w|) = %.1e ms⁻¹, wall time: %s\n",
+                                iteration(sim), prettytime(sim), prettytime(sim.Δt),
+                                maximum(abs, sim.model.velocities.w), prettytime(sim.run_wall_time))
+
+add_callback!(simulation, progress_message, IterationInterval(20))
+# Create a NamedTuple with eddy viscosity
+eddy_viscosity = (; νₑ = model.diffusivity_fields.νₑ)
+
+filename = "ocean_wind_mixing_and_convection"
+
+simulation.output_writers[:slices] =
+    JLD2OutputWriter(model, merge(model.velocities, model.tracers, eddy_viscosity),
+                     filename = filename * ".jld2",
+                     indices = (:, grid.Ny/2, :),
+                     schedule = TimeInterval(1minute),
+                     overwrite_existing = true)
+run!(simulation)
