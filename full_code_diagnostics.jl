86cfbb648b812a7b895180e9e3110c50284da651
loganpknudsen <logantheginjaninja@gmail.com>
2024-08-07 13:34:38 -0400

    Update full_code_diagnostics.jl

--- full_code_diagnostics.jl
+++ full_code_diagnostics.jl
@@ -131,7 +131,7 @@ wi(x, z) = ns*Random.randn()
 # set simulation and decide run time
 set!(model, u=ui, v=vi, w=wi)
 
-simulation = Simulation(model, Δt = 1seconds, stop_time = 0.01*((2*pi)/f)seconds) # stop_iteration=10
+simulation = Simulation(model, Δt = 1seconds, stop_time = 15.01*((2*pi)/f)seconds) # stop_iteration=10
 
 # time step wizard
 wizard = TimeStepWizard(cfl=0.95, max_change=1.1seconds, max_Δt=100.0seconds, min_Δt=0.01seconds) 
