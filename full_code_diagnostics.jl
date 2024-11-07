93ce9faa8b5565b015e5e90c8bb55f29d8ddd223
loganpknudsen <logantheginjaninja@gmail.com>
2024-11-07 13:12:45 -0500

    Update full_code_diagnostics.jl
    Fix 4 try again

--- full_code_diagnostics.jl
+++ full_code_diagnostics.jl
@@ -103,7 +103,7 @@ B_field = BackgroundField(constant_stratification, parameters=p)
 # Boundary condition set up
 # Free Slip Boundary Conditions
 
-b_bc_top= GradientBoundaryCondition=(-1*N²*(1+θ))
+b_bc_top= GradientBoundaryCondition=(-1*N²)
 # b_bc_bottom= GradientBoundaryCondition(N²)
 buoyancy_grad = FieldBoundaryConditions(top=b_bc_top) # ,bottom=b_bc_bottom
 
