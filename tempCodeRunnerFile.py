17f9100e8212aba5f9debaa322bed7d339846043
loganpknudsen <logantheginjaninja@gmail.com>
2024-11-20 21:26:54 -0500

    Updates
    Updates

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,6 +1,5 @@
-fig, ax = plt.subplots()
-# ax.set_xlabel("Wavenumber")
-# ax.set_ylabel("Growth Rate")
-# # ax.title("Growth Rate for delta = 0.5 S=0.158 and var. k")
-# ax.plot(k_list,evals[0,0,0,:]*(-1))
-# plt.show()
+mega*u-gamma**(0.5)*delta*uz*w+1j*k*gamma**(0.5)*delta*uz*one_z*u+1j*k*Ri*p-alpha*b= 0")
+# problem.add_equation("-1j*omega*v-(1+delta+delta*gamma*vz)*w+1j*k*gamma**(0.5)*delta*uz*one_z*v+u-n*theta*w= 0")
+# problem.add_equation("-1j*omega*n**2*w+1j*k*gamma**(0.5)*delta*n*uz*one_z*w+n*theta*v+Ri*pz-Ri*b= 0")
+# problem.add_equation("1j*k*u+wz=0")
+# problem.add_equation("-1j*ome
