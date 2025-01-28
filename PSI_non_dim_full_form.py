63e3ffd4fe46da69d0ad4760a980f9c0e4d0631c
loganpknudsen <logantheginjaninja@gmail.com>
2025-01-28 12:01:47 -0500

    Stability Run

--- PSI_non_dim_full_form.py
+++ PSI_non_dim_full_form.py
@@ -20,7 +20,7 @@ lmbd = N_list[0]**2*theta*(1-gm)/f
 # Basis
 coord = d3.Coordinate('z')
 dist = d3.Distributor(coord, dtype=np.complex128)
-basis = d3.Chebyshev(coord, 64, bounds=(0, H))
+basis = d3.Chebyshev(coord, 512, bounds=(0, H))
 
 # Fields
 u = dist.Field(name="u",bases=basis)
@@ -79,7 +79,7 @@ problem.add_equation("p(z="+str(H)+")=0")
 solver = problem.build_solver()
 evals = []
 gammas = []
-k_list = np.arange(0,41,2)
+k_list = np.arange(0,101,1)
 time = np.arange(0,2*np.pi/(1+N_list[0]**2*theta**2*f**(-2))**(0.5)+1,(1+N_list[0]**2*theta**2*f**(-2))**(-0.5)) # np.arange(0,2*np.pi,0.1)
 vs = []
 for ti in time:
@@ -146,12 +146,10 @@ print(np.shape(vs))
 g_index= np.linspace(0,len(gamma_list)+1,len(gamma_list))
 gr_data = xr.Dataset(data_vars={"growth_rate":(["t","N","delta","gamma_index","k"],evals[:,:,:,:,:,0]),"gamma":(["t","N","delta","gamma_index","k"],gammas[:,:,:,:,:,0])},coords={"t":time,"N":N_list,"delta":delta_list,"gamma_index":g_index,"k":k_list})
 gr_data = gr_data.mean(["t"])
-gr_data.to_netcdf("PSI_non_dim_full_form.nc")
+gr_data.to_netcdf("PSI_non_dim_full_form_high_res.nc")
 grid_normal = basis.global_grid(dist,scale=1).ravel()
 field_data = xr.Dataset({"v_structure":(["t","k","z"],vs)},coords={"t":time,"k":k_list,"z":grid_normal})
 field_data = field_data.mean(["t"])
-# z_data = xr.Dataset(grid_normal)
-field_data.to_netcdf("PSI_non_dim_field.nc")
-# z_data.to_netcdf("PSI_non_dim_z_data.nc")
+field_data.to_netcdf("PSI_non_dim_field_high_res.nc")
 
 
