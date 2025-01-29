fa781e91e656b7e82f2b3e1734265706b72589b2
loganpknudsen <logantheginjaninja@gmail.com>
2025-01-29 14:00:58 -0500

    Update PSI_non_dim_full_form.py

--- PSI_non_dim_full_form.py
+++ PSI_non_dim_full_form.py
@@ -66,7 +66,7 @@ problem.add_equation("dt(u)-delta*u_sz*w+delta*u_sz*one_z*dx(u)-v*np.cos(theta)+
 problem.add_equation("dt(v)+(1-delta*v_sz)*w+delta*u_sz*one_z*dx(v)+u*np.cos(theta)-n*np.sin(theta)*w=0")
 problem.add_equation("n**2*dt(w)+n**2*delta*u_sz*one_z*dx(w)+n*np.sin(theta)*v+Ri*pz-Ri*b*np.cos(theta)=0")
 problem.add_equation("dx(u)+wz=0")
-problem.add_equation("dt(b)+Ri**(-1)*(1+alpha)*u+(1-delta*Ri**(-1)*b_sz-Ri**(-1)*n*theta)*w+delta*u_sz*one_z*dx(b)=0") # *gamma**(-1)
+problem.add_equation("dt(b)+Ri**(-1)*(1+alpha)*u+(1-delta*Ri**(-1)*gm**(-1)*b_sz-Ri**(-1)*n*theta)*w+delta*u_sz*one_z*dx(b)=0") # *gamma**(-1)
 problem.add_equation("w(z=0)=0")
 problem.add_equation("w(z="+str(H)+")=0")
 problem.add_equation("p(z=0)=0")
@@ -80,7 +80,7 @@ evals_i =[]
 gammas = []
 k_list = np.arange(0,31,1)
 # phase = np.pi/2
-time = np.arange(0,(2*np.pi+0.1)/(1+N_list[0]**2*theta**2*f**(-2))**(0.5),0.1*(1+N_list[0]**2*theta**2*f**(-2))**(-0.5)) # np.arange(0,2*np.pi,0.1)
+time = np.arange(0,(2*np.pi+1)/(1+N_list[0]**2*theta**2*f**(-2))**(0.5),1*(1+N_list[0]**2*theta**2*f**(-2))**(-0.5)) # np.arange(0,2*np.pi,0.1)
 us = []
 vs = []
 ws = []
@@ -134,7 +134,7 @@ for ti in time:
                     idx = np.argsort(omg.imag)
                     sorted_evals = solver.eigenvalues[idx[-1]].imag
                     sorted_evals_i = solver.eigenvalues[idx[-1]].real
-                    solver.set_state(idx[-1],solver.subsystems[0])
+                    solver.set_state(idx[-1])
 
                     # print(np.shape(solver.state))
                     ui = (u['g']).real
@@ -174,9 +174,9 @@ ws = np.array(ws)
 bs = np.array(bs)
 g_index= np.linspace(0,len(gamma_list)+1,len(gamma_list))
 gr_data = xr.Dataset(data_vars={"growth_rate":(["t","N","delta","gamma_index","k"],evals_r[:,:,:,:,:,0]),"oscillation":(["t","N","delta","gamma_index","k"],evals_i[:,:,:,:,:,0]),"gamma":(["t","N","delta","gamma_index","k"],gammas[:,:,:,:,:,0])},coords={"t":time,"N":N_list,"delta":delta_list,"gamma_index":g_index,"k":k_list})
-gr_data.to_netcdf("PSI_non_dim_full_form_low_res_no_gm.nc") 
+gr_data.to_netcdf("PSI_non_dim_full_form_low_res.nc") 
 grid_normal = basis.global_grid(dist,scale=1).ravel()
 field_data = xr.Dataset({"u_structure":(["t","k","z"],us),"v_structure":(["t","k","z"],vs),"w_structure":(["t","k","z"],ws),"b_structure":(["t","k","z"],bs)},coords={"t":time,"k":k_list,"z":grid_normal})
-field_data.to_netcdf("PSI_non_dim_field_low_res_no_gm.nc")
+field_data.to_netcdf("PSI_non_dim_field_low_res.nc")
 
 
