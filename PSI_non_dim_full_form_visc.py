78e5bd78c22ad90462c65f1af1d810fa2bda8fb1
loganpknudsen <logantheginjaninja@gmail.com>
2025-02-04 14:39:49 -0500

    Boundary Phrasing Experiement

--- PSI_non_dim_full_form_visc.py
+++ PSI_non_dim_full_form_visc.py
@@ -46,6 +46,7 @@ tau_6 = dist.Field(name="tau_6")
 tau_7 = dist.Field(name="tau_7")
 tau_8 = dist.Field(name="tau_8")
 tau_p = dist.Field(name="tau_p") # pressure gauge
+tau_p1 = dist.Field(name="tau_p1") # pressure gauge
 omega = dist.Field(name="omega")
 Ek = dist.Field(name="Ek") # Ekman Number
 
@@ -79,11 +80,11 @@ dz = lambda A: d3.Differentiate(A, coord)
 
 
 # Problem
-problem = d3.EVP([u,uz,uzz,v,vz,vzz,w,wz,wzz,b,bz,bzz,p,tau_1,tau_2,tau_3,tau_4,tau_5,tau_6,tau_7,tau_8,tau_p], eigenvalue=omega, namespace=locals()) # 
+problem = d3.EVP([u,uz,uzz,v,vz,vzz,w,wz,wzz,b,bz,bzz,p,tau_1,tau_2,tau_3,tau_4,tau_5,tau_6,tau_7,tau_8,tau_p,tau_p1], eigenvalue=omega, namespace=locals()) # 
 
 problem.add_equation("dt(u)-delta*u_sz*w+delta*u_sz*one_z*dx(u)-v*np.cos(theta)+Ri*dx(p)-alpha*b*np.cos(theta)-Ek*uzz+lift(tau_2)= 0")
 problem.add_equation("dt(v)+(1-delta*v_sz)*w+delta*u_sz*one_z*dx(v)+u*np.cos(theta)-n*np.sin(theta)*w-Ek*vzz+lift(tau_4)=0")
-problem.add_equation("n**2*dt(w)+n**2*delta*u_sz*one_z*dx(w)+n*np.sin(theta)*v+Ri*dz(p)+lift(tau_p)-Ri*b*np.cos(theta)-n**(2)*Ek*wzz+lift(tau_6)=0") 
+problem.add_equation("n**2*dt(w)+n**2*delta*u_sz*one_z*dx(w)+n*np.sin(theta)*v+Ri*dz(p)+lift(tau_p)+lift(tau_p1)-Ri*b*np.cos(theta)-n**(2)*Ek*wzz+lift(tau_6)=0") 
 problem.add_equation("dx(u)+wz=0")
 problem.add_equation("dt(b)+Ri**(-1)*(1+alpha)*u*np.cos(theta)+(1-delta*Ri**(-1)*gamma**(-1)*b_sz-Ri**(-1)*n*np.tan(theta))*w*np.cos(theta)+delta*u_sz*one_z*dx(b)-Ek*bzz+lift(tau_8)=0") # *gamma**(-1)
 problem.add_equation("uz- dz(u)-lift(tau_1)=0")
@@ -101,7 +102,9 @@ problem.add_equation("v(z=0)=0")
 problem.add_equation("v(z="+str(H)+")=0")
 problem.add_equation("w(z=0)=0")
 problem.add_equation("w(z="+str(H)+")=0")
-problem.add_equation("integ(p)=0")
+# problem.add_equation("integ(p)=0")
+problem.add_equation("p(z=0)=0")
+problem.add_equation("p(z="+str(H)+")=0")
 problem.add_equation("b(z=0)=0")
 problem.add_equation("b(z="+str(H)+")=0")
 
@@ -236,9 +239,9 @@ wsc = np.array(wsc)
 bsc = np.array(bsc)
 g_index= np.linspace(0,len(gamma_list)+1,len(gamma_list))
 gr_data = xr.Dataset(data_vars={"growth_rate":(["t","N","delta","gamma_index","k"],evals_r[:,:,:,:,:,0]),"oscillation":(["t","N","delta","gamma_index","k"],evals_i[:,:,:,:,:,0]),"gamma":(["t","N","delta","gamma_index","k"],gammas[:,:,:,:,:,0])},coords={"t":time,"N":N_list,"delta":delta_list,"gamma_index":g_index,"k":k_list})
-gr_data.to_netcdf("PSI_non_dim_full_form_visc_low_res_low_t.nc") 
+gr_data.to_netcdf("PSI_non_dim_full_form_visc_low_res_exprmnt.nc") 
 grid_normal = basis.global_grid(dist,scale=1).ravel()
 field_data = xr.Dataset({"u_structure":(["t","k","z"],us),"v_structure":(["t","k","z"],vs),"w_structure":(["t","k","z"],ws),"b_structure":(["t","k","z"],bs),"u_structure_complex":(["t","k","z"],usc),"v_structure_complex":(["t","k","z"],vsc),"w_structure_complex":(["t","k","z"],wsc),"b_structure_complex":(["t","k","z"],bsc)},coords={"t":time,"k":k_list,"z":grid_normal})
-field_data.to_netcdf("PSI_non_dim_field_low_res_low_t.nc")
+field_data.to_netcdf("PSI_non_dim_field_low_res_low_exprmnt.nc")
 
 
