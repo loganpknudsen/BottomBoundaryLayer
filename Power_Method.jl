e178437e2a0519a43672ea5ef812aa1d96f436c2
loganpknudsen <logantheginjaninja@gmail.com>
2025-02-28 13:28:32 -0500

    Summary

--- Power_Method.jl
+++ Power_Method.jl
@@ -170,15 +170,15 @@ in the estimated growth rate ``σ`` falls below `convergence_criterion`.
 Returns ``σ``.
 """
 function estimate_growth_rate(simulation, energy, convergence_criterion=1e-3)
-    σ = CuArray([])
-    power_method_data = CuArray([])
-    hcat!(power_method_data, (σ=deepcopy(σ)))
+    σ = []
+    power_method_data = []
+    push!(power_method_data, (σ=deepcopy(σ)))
 
     while convergence(σ) > convergence_criterion
         compute!(energy)
 
         @info @sprintf("About to start power method iteration %d; kinetic energy: %.2e", length(σ)+1, energy)
-        hcat!(σ, grow_instability!(simulation, energy))
+        push!(σ, grow_instability!(simulation, energy))
         compute!(energy)
 
         @info @sprintf("Power method iteration %d, kinetic energy: %.2e, σⁿ: %.2e, relative Δσ: %.2e",
@@ -186,7 +186,7 @@ function estimate_growth_rate(simulation, energy, convergence_criterion=1e-3)
 
         compute!(ω)
         rescale!(simulation.model, energy)
-        hcat!(power_method_data, (σ=deepcopy(σ)))
+        push!(power_method_data, (σ=deepcopy(σ)))
     end
 
     return σ, power_method_data
