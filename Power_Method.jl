8b818d898eff2baecd97b3cc8daa111abc26bf83
loganpknudsen <logantheginjaninja@gmail.com>
2025-03-02 17:00:34 -0500

    Update Power_Method.jl

--- Power_Method.jl
+++ Power_Method.jl
@@ -118,15 +118,16 @@ model = NonhydrostaticModel(; grid, buoyancy, coriolis, closure,
                             boundary_conditions = (; b=buoyancy_grad),
                             background_fields = (; u=U_field, v=V_field, b=B_field))
 
-simulation = Simulation(model, Δt = 1seconds, stop_time = 8.0*((2*pi)/f)seconds, verbose=false)
+const original_stop_time = 5.0*((2*pi)/f)seconds
+simulation = Simulation(model, Δt = 1seconds, stop_time = original_stop_time, verbose=false)
 
 wizard = TimeStepWizard(cfl=0.95, max_change=1.1seconds, max_Δt=100.0seconds, min_Δt=0.01seconds) 
 simulation.callbacks[:wizard] = Callback(wizard, IterationInterval(10)) 
 
 function grow_instability!(simulation, energy)
     # Initialize
-    simulation.model.clock.iteration = 8.0*((2*pi)/f)seconds
-    simulation.model.stop_time = 10.0*((2*pi)/f)seconds
+    simulation.model.clock.iteration = original_stop_time
+    simulation.model.stop_time = original_stop_time + 2.0*((2*pi)/f)seconds
     t₀ = simulation.model.clock.time = 0
     compute!(energy)
     energy₀ = CUDA.@allowscalar energy[1,1,1]
