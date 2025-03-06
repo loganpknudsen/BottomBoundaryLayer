7718787ede8eb52ccb4626cbbc7bbe0b7b0a882b
loganpknudsen <logantheginjaninja@gmail.com>
2025-03-05 19:13:14 -0500

    Summary

--- full_code_diagnostics_no_w.jl
+++ full_code_diagnostics_no_w.jl
@@ -217,4 +217,4 @@ simulation.output_writers[:diagnostics] = NetCDFOutputWriter(model, output2;
 # With initial conditions set and an output writer at the ready, we run the simulation
 # simulation.stop_time = 15*((2π)/f)seconds
 
-run!(simulation
+run!(simulation) # , pickup=true
