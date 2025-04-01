5c406d1a93afe7189c71847c7bafb1f2363f0f6d
loganpknudsen <logantheginjaninja@gmail.com>
2025-03-31 20:24:19 -0400

    Summary

--- full_code_diagnostics.jl
+++ full_code_diagnostics.jl
@@ -51,7 +51,7 @@ grid = RectilinearGrid(arch; topology = (Periodic, Flat, Bounded),
 
 
 # tilted domain parameters
-const θ = 5e-2 # degrees 10^(-2) is previous value for 110 meter layer
+const θ = 5e-3 # degrees 10^(-2) is previous value for 110 meter layer
 ĝ = [sind(θ), 0, cosd(θ)] # gravity vector
 
 # realistic mid latitude for now
@@ -59,9 +59,9 @@ buoyancy = Buoyancy(model = BuoyancyTracer(), gravity_unit_vector = -ĝ)
 coriolis = ConstantCartesianCoriolis(f = 1e-4, rotation_axis = ĝ)
 
 # parameters for simulation
-const V∞ = 0.02 # m s⁻¹ interior velocity
+const V∞ = 0.05 # m s⁻¹ interior velocity
 const f = 1e-4 # coriolis parameter
-const N² = 1e-7 # interior stratification
+const N² = 1e-5 # interior stratification
 const S∞ = (N²*θ^2)/(f^2) # slope burger number
 const fˢ = (f^2+θ^2*N²)^(0.5) # modified oscillation
 const δ = 0.5
@@ -105,7 +105,7 @@ b_bc_top= GradientBoundaryCondition(-1*N²)
 buoyancy_grad = FieldBoundaryConditions(top=b_bc_top) 
 
 # diffusitivity and viscosity values for closure
-const ν1 = 1e-6
+const ν1 = 1e-4
 closure = ScalarDiffusivity(ν=ν1, κ=ν1)
 
 start_time = time_ns()
