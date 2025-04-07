215211273bd1ab105f79ecf37fbf3e977fbffff8
loganpknudsen <logantheginjaninja@gmail.com>
2025-04-07 18:37:11 -0400

    Update parameters.jl

--- parameters.jl
+++ parameters.jl
@@ -6,16 +6,16 @@ using Parameters
 
     # Ht: shelf region height, Lt: slopelength, Lb: length of sfc cooling flux
     f1e4N21e5threta029gammau = (
-        f=1e-4,
+        f = 1e-4,
         N² = 1e-5,               # maximum wave velocity
         θ = 0.29,
-        S∞ = (N²*tand(θ)^2)/(f^2),
-        γ = (cosd(θ)*(1+S∞*(1-δ)))^(-1)
+        S = (N²*tand(θ)^2)/(f^2),
+        γ = (cosd(θ)*(1+S*(1-δ)))^(-1)
     )
     f1e4N21e5threta029gammal = (
-        f=1e-4,
+        f = 1e-4,
         N² = 1e-5,               # maximum wave velocity
         θ = 0.29,
-        S∞ = (N²*tand(θ)^2)/(f^2),
-        γ = (cosd(θ)*(1+S∞*(1-δ)))^(-1)
+        S = (N²*tand(θ)^2)/(f^2),
+        γ = (cosd(θ)*(1+S*(1-δ)))^(-1)
     )
