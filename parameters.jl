5fc5886fc7854ede5911696f291e1ba8ae5bb018
loganpknudsen <logantheginjaninja@gmail.com>
2025-04-07 18:40:41 -0400

    Update parameters.jl

--- parameters.jl
+++ parameters.jl
@@ -9,13 +9,13 @@ using Parameters
         f = 1e-4,
         N² = 1e-5,               # maximum wave velocity
         θ = 0.29,
-        S = (N²*tand(θ)^2)/(f^2),
-        γ = (cosd(θ)*(1+S*(1-δ)))^(-1)
+        S∞ = (N²*tand(θ)^2)/(f^2),
+        γ = (cosd(θ)*(1+S∞*(1-δ)))^(-1),
     )
     f1e4N21e5threta029gammal = (
         f = 1e-4,
         N² = 1e-5,               # maximum wave velocity
         θ = 0.29,
-        S = (N²*tand(θ)^2)/(f^2),
-        γ = (cosd(θ)*(1+S*(1-δ)))^(-1)
+        S∞ = (N²*tand(θ)^2)/(f^2),
+        γ = (cosd(θ)*(1+S∞*(1-δ)))^(-1),
     )
