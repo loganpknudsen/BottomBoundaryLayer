eeadfd9262ed3ce37f0d8713dc4f1cea1549c6fa
loganpknudsen <logantheginjaninja@gmail.com>
2025-04-08 01:32:17 -0400

    Update full_code_diagnostics.jl

--- full_code_diagnostics.jl
+++ full_code_diagnostics.jl
@@ -1,7 +1,6 @@
 ### Load in Packages
 using Pkg
 Pkg.activate(".")
-Pkg.instantiate(".")
 using Oceananigans
 using Oceananigans.AbstractOperations: @at, ∂x, ∂y, ∂z
 using Oceananigans.AbstractOperations: @at, Average
