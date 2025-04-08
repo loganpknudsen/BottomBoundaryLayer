1b0eecc28351f5a0d36578ab357a219366fe015b
loganpknudsen <logantheginjaninja@gmail.com>
2025-04-08 00:27:57 -0400

    Update parameters.jl

--- parameters.jl
+++ parameters.jl
@@ -5,7 +5,7 @@ using Parameters
     T = 30
 
     # Ht: shelf region height, Lt: slopelength, Lb: length of sfc cooling flux
-    f1e4N21e5theta029gammau = (f = 1e-4,
+    f1e4theta029N21e5gammau = (f = 1e-4,
         N²= 1e-5,               # maximum wave velocity
         θ = 0.29,
         δ = δ,
@@ -15,7 +15,7 @@ using Parameters
         γ = (cosd(0.29)*(1+(1e-5*tand(0.29)^2)/((1e-4)^2)*(1-δ)))^(-1),
     )
 
-    f1e4N21e5theta029gammal = (f = 1e-4,
+    f1e4theta029N21e5gammal = (f = 1e-4,
         N² = 1e-5,               # maximum wave velocity
         θ = 0.29,
         δ = δ,
@@ -24,7 +24,7 @@ using Parameters
         S∞ = (1e-5*tand(0.29)^2)/((1e-4)^2),
         γ = (3-(1e-5*tand(0.29)^2)/((1e-4)^2))*(cosd(0.29)*(3*(1+(1e-5*tand(0.29)^2)/((1e-4)^2))-2*(1e-5*tand(0.29)^2)/((1e-4)^2)))^(-1),
     )
-    f1e4N21e6theta1gammau = (f = 1e-4,
+    f1e4theta1N21e6gammau = (f = 1e-4,
         N² = 1e-5,               # maximum wave velocity
         θ = 1,
         δ = δ,
@@ -33,7 +33,7 @@ using Parameters
         S∞ = (1e-5*tand(1)^2)/((1e-4)^2),
         γ = (cosd(1)*(1+(1e-5*tand(1)^2)/((1e-4)^2)*(1-δ)))^(-1),
     )
-    f1e4N21e6theta6gammau = (f = 1e-4,
+    f1e4theta6N21e6gammau = (f = 1e-4,
         N² = 1e-6,               # maximum wave velocity
         θ = 6,
         δ = δ,
@@ -42,7 +42,7 @@ using Parameters
         S∞ = (1e-6*tand(6)^2)/((1e-4)^2),
         γ = (cosd(6)*(1+(1e-6*tand(6)^2)/((1e-4)^2)*(1-δ)))^(-1),
     )
-    f1e4N21e7theta05gammau = (f = 1e-4,
+    f1e4theta05N21e7gammau = (f = 1e-4,
         N² = 1e-7,               # maximum wave velocity
         θ = 0.5,
         δ = δ,
@@ -51,7 +51,7 @@ using Parameters
         S∞ = (1e-7*tand(0.5)^2)/((1e-4)^2),
         γ = (cosd(0.5)*(1+(1e-7*tand(0.5)^2)/((1e-4)^2)*(1-δ)))^(-1),
     )
-    f1e4N21e5theta0009gammau = (f = 1e-4,
+    f1e4theta0009N21e5gammau = (f = 1e-4,
         N² = 1e-5,               # maximum wave velocity
         θ = 0.009,
         δ = δ,
