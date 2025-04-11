5a664997ee6c50fa2207ce0152d707d83bdd255d
loganpknudsen <logantheginjaninja@gmail.com>
2025-04-11 16:02:21 -0400

    Summary

--- full_code_diagnostics_back_up_copy.jl
+++ full_code_diagnostics_back_up_copy.jl
@@ -40,9 +40,9 @@ arch = has_cuda_gpu() ? GPU() : CPU()
 @info("Arch => $arch")
 
 Lx = 3000meters
-Lz = 100meters
+Lz = 200meters
 Nx = 1024 #1024 # 512 originally
-Nz = 128 #256 #128 # # 128 originally Note to self, maintain 2 to 1 resolution ration
+Nz = 256 #256 #128 # # 128 originally Note to self, maintain 2 to 1 resolution ration
 
 grid = RectilinearGrid(arch; topology = (Periodic, Flat, Bounded),
                        size = (Nx, Nz),
@@ -51,7 +51,7 @@ grid = RectilinearGrid(arch; topology = (Periodic, Flat, Bounded),
 
 
 # tilted domain parameters
-const θ = 0.5 # degrees 10^(-2) is previous value for 110 meter layer
+const θ = 2 # degrees 10^(-2) is previous value for 110 meter layer
 const f = 1e-4
 ĝ = [sind(θ), 0, cosd(θ)] # gravity vector
 
@@ -60,9 +60,9 @@ buoyancy = Buoyancy(model = BuoyancyTracer(), gravity_unit_vector = -ĝ)
 coriolis = ConstantCartesianCoriolis(f = f, rotation_axis = ĝ)
 
 # parameters for simulation
-const V∞ = 0.005 # m s⁻¹ interior velocity
+const V∞ = 0.1 # m s⁻¹ interior velocity
 const f = 1e-4 # coriolis parameter
-const N² = 1e-6 # interior stratification
+const N² = 1e-5 # interior stratification
 const S∞ = (N²*tand(θ)^2)/(f^2) # slope burger number
 const fˢ = cosd(θ)*(f^2+tand(θ)^2*N²)^(0.5) # modified oscillation
 const δ = 0.5
