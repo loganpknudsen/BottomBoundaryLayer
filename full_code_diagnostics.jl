3bc3b4f3c444394a3aad8a6eafdb4f229cb7c942
loganpknudsen <logantheginjaninja@gmail.com>
2025-04-14 04:19:00 -0400

    Update full_code_diagnostics.jl

--- full_code_diagnostics.jl
+++ full_code_diagnostics.jl
@@ -136,7 +136,7 @@ b_bc_top= GradientBoundaryCondition(-1*N²*cosd(θ))
 buoyancy_grad = FieldBoundaryConditions(top = b_bc_top) 
 
 # diffusitivity and viscosity values for closure
-const ν1 = 1e-5
+const ν1 = 1e-4
 closure = ScalarDiffusivity(ν=ν1, κ=ν1)
 
 start_time = time_ns()
@@ -149,7 +149,7 @@ model = NonhydrostaticModel(; grid, buoyancy, coriolis, closure,
                             boundary_conditions = (; b=buoyancy_grad),
                             background_fields = (; u=U_field, v=V_field, b=B_field))
 
-ns = V∞*10^(-2) # standard deviation for noise
+ns = V∞*10^(-6) # standard deviation for noise
 
 # initial conditions to start instability
 ui(x, z) = ns*Random.randn()
