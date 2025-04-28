5174d96d2f2d3c7d63865f2231fc33c84217f2e1
loganpknudsen <logantheginjaninja@gmail.com>
2025-04-28 03:43:03 -0400

    Fix ktransport

--- diagnostics.jl
+++ diagnostics.jl
@@ -5,7 +5,13 @@ using Oceananigans.AbstractOperations: KernelFunctionOperation
 using Oceananigans.Grids: Center, Face
 using Oceananigans.Fields: ZeroField
 using Oceananigans.Models.NonhydrostaticModels: u_velocity_tendency, v_velocity_tendency, w_velocity_tendency
-using Oceananigans.TurbulenceClosures: ∂ⱼ_τ₁ⱼ, ∂ⱼ_τ₂ⱼ, ∂ⱼ_τ₃ⱼ
+using Oceananigans.Advection: div_𝐯u, div_𝐯v, div_𝐯w
+using Oceananigans.TurbulenceClosures: viscous_flux_ux, viscous_flux_uy, viscous_flux_uz,
+                                       viscous_flux_vx, viscous_flux_vy, viscous_flux_vz,
+                                       viscous_flux_wx, viscous_flux_wy, viscous_flux_wz,
+                                       ∂ⱼ_τ₁ⱼ, ∂ⱼ_τ₂ⱼ, ∂ⱼ_τ₃ⱼ
+using Oceananigans.TurbulenceClosures: immersed_∂ⱼ_τ₁ⱼ, immersed_∂ⱼ_τ₂ⱼ, immersed_∂ⱼ_τ₃ⱼ
+using Oceananigans.BuoyancyFormulations: x_dot_g_bᶠᶜᶜ, y_dot_g_bᶜᶠᶜ, z_dot_g_bᶜᶜᶠ
 
 using Oceanostics: _νᶜᶜᶜ
 using Oceanostics: validate_location, validate_dissipative_closure, perturbation_fields
@@ -15,13 +21,14 @@ using Oceanostics: validate_location, validate_dissipative_closure, perturbation
 @inline ψ²(i, j, k, grid, ψ) = @inbounds ψ[i, j, k]^2
 
 @inline ψ′²(i, j, k, grid, ψ, ψ̄) = @inbounds (ψ[i, j, k] - ψ̄[i, j, k])^2
+@inline ψ′(i, j, k, grid, ψ, ψ̄) = @inbounds (ψ[i, j, k] - ψ̄[i, j, k])
 @inline ψ′²(i, j, k, grid, ψ, ψ̄::Number) = @inbounds (ψ[i, j, k] - ψ̄)^2
 
 @inline fψ²(i, j, k, grid, f, ψ) = f(i, j, k, grid, ψ)^2
 
 @inline fψ_plus_gφ²(i, j, k, grid, f, ψ, g, φ) = (f(i, j, k, grid, ψ) + g(i, j, k, grid, φ))^2
 
-@inline ψf(i, j, k, grid, ψ, f, args...) = @inbounds ψ[i, j, k] * f(i, j, k, grid, args...)
+@inline ψf(i, j, k, grid, ψ, f) = @inbounds ψ[i, j, k] * f[i, j, k]
 
 @inline function TRNS(i, j, k, grid, closure,
                                             diffusivity_fields,
@@ -30,7 +37,8 @@ using Oceanostics: validate_location, validate_dissipative_closure, perturbation
                                             buoyancy,
                                             mean_velocities)
     k = (ℑxᶜᵃᵃ(i, j, k, grid, ψ′², model.velocities.u, mean_velocities.U) + ℑyᵃᶜᵃ(i, j, k, grid, ψ′², model.velocities.v, mean_velocities.V) + ℑzᵃᵃᶜ(i, j, k, grid, ψ′², model.velocities.w, mean_velocities.W)) / 2
-    wk = ℑzᵃᵃᶜ(i, j, k, grid, ψf, model.velocities.w-mean_velocities.W, k)
+    wpert = ℑzᵃᵃᶜ(i, j, k, grid, ψ′, model.velocities.w, mean_velocities.W)
+    wk = ψf(i,j,k,grid, wpert,k)
     dwkdz = ∂zᶜᶜᶜ(i, j, k, grid, wk)
     return dwkdz
 end
