4aca38419966026f4cd1c017acbc039aa49429ca
loganpknudsen <logantheginjaninja@gmail.com>
2025-06-20 15:36:48 -0400

    Update full_code_diagnostics_back_up_copy.jl

--- full_code_diagnostics_back_up_copy.jl
+++ full_code_diagnostics_back_up_copy.jl
@@ -42,7 +42,7 @@ arch = has_cuda_gpu() ? GPU() : CPU()
 Lx = 2000meters
 Lz = 150meters
 Nx = 1024 #1024 # 512 originally
-Nz = 128*1.5 #256 #128 # # 128 originally Note to self, maintain 2 to 1 resolution ration
+Nz = 192 #256 #128 # # 128 originally Note to self, maintain 2 to 1 resolution ration
 
 grid = RectilinearGrid(arch; topology = (Periodic, Flat, Bounded),
                        size = (Nx, Nz),
@@ -237,4 +237,4 @@ simulation.output_writers[:diagnostics] = NetCDFOutputWriter(model, output2;
 # With initial conditions set and an output writer at the ready, we run the simulation
 # simulation.stop_time = 15*((2π)/f)seconds
 
-run!(simulation) # , pickup=true
+run!(simulation) # , pickup=tru
