874d3d68c9ff59b7bff49e59e48bd73c0f7b91ce
loganpknudsen <logantheginjaninja@gmail.com>
2025-06-29 21:11:39 -0400

    Update full_code_diagnostics_stblty_comp.jl

--- full_code_diagnostics_stblty_comp.jl
+++ full_code_diagnostics_stblty_comp.jl
@@ -61,7 +61,7 @@ end
 
 ### Path Saved To
 
-path_name = "/glade/derecho/scratch/knudsenl/data/new_data/final_paper/" # args["path"]
+path_name = "/glade/derecho/scratch/knudsenl/data/new_data/stbly/" # args["path"]
 setname = args["suffix"]
 
 ### Load in Parameters
