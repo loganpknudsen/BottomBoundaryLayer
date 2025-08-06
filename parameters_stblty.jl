7c5953ba21dbc0a0efca7b3997718a19b09230de
loganpknudsen <logantheginjaninja@gmail.com>
2025-08-06 14:22:59 -0400

    Hopefully final debug

--- parameters_stblty.jl
+++ parameters_stblty.jl
@@ -1,12 +1,9 @@
 using Parameters
 
-f = 1e-4
-θ = 0.01
-
 @with_kw struct DenseParams
 
     S01gammau = (S = 0.1,
-        N² = (0.1)^2*f^2/θ^2,
+        N² = (0.1)^2*(1e-4)^2/(0.01)^2,
         ϕ = pi/2,
         δ = 0.5,
         T = 1,
