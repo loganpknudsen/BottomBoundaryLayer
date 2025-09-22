b0d35415bceb7d6344496017d7ed92dccb89f5c5
loganpknudsen <logantheginjaninja@gmail.com>
2025-09-21 22:53:11 -0700

    Summary

--- stability_analysis_full_run_small_angle.py
+++ stability_analysis_full_run_small_angle.py
@@ -28,7 +28,7 @@ dt = 250
 t = np.linspace(0, tau+1/dt, dt)
 dm = 1
 m = np.arange(-30, 5+dm, dm)
-dgm = 50
+dgm = 20
 ddelta = 50
 delta_list = np.linspace(0,1+1/ddelta,ddelta)
 for S in S_list:
@@ -44,8 +44,8 @@ for S in S_list:
         N2 = S2*f**2/theta**2
         beta = (1+S2)**(0.5)
         fstar = f*beta
-        gml = (1+(1-10/7)*S2)/(1+S2) 
-        gmu = (1+(1-10/6)*S2)/(1+S2)
+        gml = (1+(1-10/6)*S2)/(1+S2) 
+        gmu = (1+(1-10/7)*S2)/(1+S2)
         gm_list = np.linspace(gml,gmu,dgm)
         gms_2.append(gm_list)
         for gm in gm_list:
