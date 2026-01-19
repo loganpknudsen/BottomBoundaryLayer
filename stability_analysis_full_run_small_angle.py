229ac0a2c05546d13a3ce57431f90292d2477b47
loganpknudsen <logantheginjaninja@gmail.com>
2026-01-19 03:25:40 -0600

    Update stability_analysis_full_run_small_angle.py

--- stability_analysis_full_run_small_angle.py
+++ stability_analysis_full_run_small_angle.py
@@ -85,4 +85,4 @@ output_file = xr.Dataset({"growth_rate":(["slope_burger_number","delta","strat_i
            "strat_values":(["slope_burger_number","delta","strat_index"],gms)},
            coords = {"slope_burger_number":S_list,"delta":delta_list,"strat_index":np.linspace(0,1,dgm)})
 
-output_file.to_netcdf("stability_analysis_gamma_Sinf_high_res.nc")
+output_file.to_netcdf("stability_analysis_gamma_Sinf_higher_res.nc")
