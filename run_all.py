7d017734c1a2d995ce4ddd947a2ed533d2a9de53
loganpknudsen <logantheginjaninja@gmail.com>
2026-07-22 16:58:16 -0500

    Drag Conditions

--- run_all.py
+++ run_all.py
@@ -19,8 +19,8 @@ all_sims = [#'S025-R065-delta08',
             # 'S10-R065-delta08',
             # 'S10-R075-delta08',
             # 'S10-R085-delta08',
-            # 'S10-R095-delta08',
-            'S10-R10-delta08',
+            'S10-R095-delta08',
+            # 'S10-R10-delta08',
             # 'S10-R105-delta08',
             # 'S15-R075-delta08',
             # 'S15-R085-delta08',
@@ -84,7 +84,7 @@ f = 1e-4
     
 verbose = 2
 aux_filename = "aux_pbs_psi.sh"
-julia_file = "full_code_diagnostics_stblty_comp.jl"
+julia_file = "full_code_diagnostics_stblty_comp_drag.jl"
 savepath = "/glade/derecho/scratch/knudsenl/data/new_data/"
 
 
